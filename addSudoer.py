8483952e88765556ebe2e1084857f97b51df088c
Daniel Hewitt <danhtemp@gmail.com>
2014-05-06 01:16:55 +0100

    Fixed an indentation error in addSudoer

--- addSudoer.py
+++ addSudoer.py
@@ -2,16 +2,16 @@ import sys
 import sqlite3
 
 # Add a sudoer to the specified database
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
 	sys.exit('Please supply a database and player name')
-	
- dbName = sys.argv[1]
- sudoer = sys.argv[2].lower()
- 
- DB = sqlite3.connect(dbName)
- c = DB.cursor()
- 
- c.execute("INSERT INTO sudoers WHERE player=?", (sudoer,))
- DB.commit()
- DB.close()
+
+dbName = sys.argv[1]
+sudoer = sys.argv[2].lower()
+
+DB = sqlite3.connect(dbName)
+c = DB.cursor()
+
+c.execute("INSERT INTO sudoers (player) VALUES (?)", (sudoer,))
+DB.commit()
+DB.close()
  
